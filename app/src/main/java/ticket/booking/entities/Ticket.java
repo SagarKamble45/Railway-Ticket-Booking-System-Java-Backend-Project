31359204bbabbfebbab2d77af5159542ae152a97
Sagar Kamble <kamblesagar572002@gmail.com>
2025-12-21 19:31:22 +0530

    Entitey created

---
+++ app/src/main/java/ticket/booking/entities/Ticket.java
@@ -0,0 +1,14 @@
+package ticket.booking.entities;
+
+import java.util.Date;
+
+public class Ticket {
+
+    private String ticketId;
+    private String userId;
+    private Date date;
+    private String source;
+    private String destination;
+    private Train train;
+
+}
---
+++ app/src/main/java/ticket/booking/services/userBookingService.java
@@ -0,0 +1,13 @@
+package ticket.booking.services;
+
+import ticket.booking.entities.User;
+
+public class userBookingService {
+
+    private User user;
+
+    public userBookingService(User user) {
+
+    }
+
+}

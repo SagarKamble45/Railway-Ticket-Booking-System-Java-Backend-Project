f757c28d06459f4361bf404cefebfa73823843ee
Sagar Kamble <kamblesagar572002@gmail.com>
2026-01-31 16:19:34 +0530

    Methods created in the userBookingService file

--- app/src/main/java/ticket/booking/services/userBookingService.java
+++ app/src/main/java/ticket/booking/services/userBookingService.java
@@ -1,13 +1,49 @@
 package ticket.booking.services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import ticket.booking.entities.User;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 
 public class userBookingService {
 
     private User user;
+    private List<User> userList;
 
-    public userBookingService(User user) {
+    private ObjectMapper objectMapper = new ObjectMapper();
 
+    private static final String USERS_PATH="../localDB/users.json";
+
+
+    public userBookingService(User user1) throws IOException {
+        this.user = user1;
+        File users = new File(USERS_PATH);
+        userList = objectMapper.readValue(users, new TypeReference<List<User>>() {});
+
+    }
+
+    // To check user is login or not
+    public Boolean loginUser(){
+        Optional<User> foundUser = userList.stream().filter(
+                user1 -> {
+                    return user1.getName().equals(user.getName()) &&
+                            UserServiceUtil.checkPassword(user.getPassword(),user1.getPassword(),user.getHashPassword(),user1.getHashPassword());
+                }).findFirst();
+        return foundUser.isPresent();
+    }
+
+    public Boolean signUp (User user1){
+        try{
+            userList.add(user1);
+            saveUserListToFile();
+            return Boolean.TRUE;
+        }catch (IOException ex){
+            return Boolean.FALSE;
+        }
     }
 
 }
